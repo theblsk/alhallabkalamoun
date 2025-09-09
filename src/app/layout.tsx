3c98bf041a5a0efdceacb002963464546afda86b
Wajdi Ballout <wajdi.ballout@blsk.dev>
2025-09-10 00:54:59 +0300

    chore: update dependencies and modify layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "Al Hallab Kalamoun",
+  description:
+    "Al Hallab Kalamoun Website to find the best Lebanese sweets right here in Kalamoun",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -22,5 +22,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
